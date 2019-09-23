a9e3018f1689b32b5566debc3052150d6c3ca8e4
cdy1996 <chendongyi1996@gmail.com>
2019-09-23 19:57:29 +0800

    说明: golang 学习

--- src/demo/panicDemo.go
+++ src/demo/panicDemo.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
+	//value6:=interface{}(byte(127))
+
 	defer func() {
 		err := recover()
 		if err != nil {
@@ -13,7 +15,11 @@ func main() {
 	error1 := testError1()
 	fmt.Println(error1)
 }
-func testError1() string{
+func testError1() string {
+	return testError2()
+}
+
+func testError2() string {
 	panic("runtime error")
 	return "ok"
 }

a9236aa48d34eaf32b547c9a377682612f7ba20c
cdy1996 <chendongyi1996@gmail.com>
2019-11-29 11:11:59 +0800

    说明: golang 短链接服务

--- go.mod
+++ go.mod
@@ -6,11 +6,12 @@ require (
 	github.com/codegangsta/inject v0.0.0-20150114235600-33e0aa1cb7c0
 	github.com/garyburd/redigo v1.6.0 // indirect
 	github.com/go-playground/universal-translator v0.17.0 // indirect
-	github.com/go-redis/redis v6.15.6+incompatible // indirect
+	github.com/go-redis/redis v6.15.6+incompatible
 	github.com/golang/protobuf v1.3.2
 	github.com/gorilla/mux v1.7.3
 	github.com/justinas/alice v1.2.0
 	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/pilu/go-base62 v0.0.0-20131214171223-d5f2747573ce
 	golang.org/x/net v0.0.0-20191014212845-da9a3fd4c582
 	google.golang.org/grpc v1.24.0
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
--- src/short-url/redis.go
+++ src/short-url/redis.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"crypto/hmac"
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"github.com/go-redis/redis"
+	"github.com/pilu/go-base62"
 	"time"
 )
 
@@ -14,7 +19,7 @@ const (
 	// mapping the shortlink to the url
 	ShorlinkKey = "shortlink:%s:url"
 	// mapping the hash of the url to the shortlink
-	URLHashKey = "rulhash:%s:url"
+	URLHashKey = "urlhash:%s:url"
 	// mapping the shorlink to  the detail of url
 	ShortlinkDetailKey = "shortlink:%s:detail"
 )
@@ -46,16 +51,16 @@ func (r *RedisCli) Shorten(url string, exp int64) (string, error) {
 		return "", err
 
 	}
-	eid := Encode(int(id))
+	eid := base62.Encode(int(id))
 
 	// 短地址 -> 真实地址
-	if err = r.Cli.Set(fmt.Sprint(ShorlinkKey, eid), url, time.Minute*time.Duration(exp)).Err(); err != nil {
+	if err = r.Cli.Set(fmt.Sprintf(ShorlinkKey, eid), url, time.Minute*time.Duration(exp)).Err(); err != nil {
 		return "", err
 
 	}
 
 	// hash(真实地址) -> 短地址
-	if err = r.Cli.Set(fmt.Sprint(URLHashKey, h), eid, time.Minute*time.Duration(exp)).Err(); err != nil {
+	if err = r.Cli.Set(fmt.Sprintf(URLHashKey, h), eid, time.Minute*time.Duration(exp)).Err(); err != nil {
 		return "", err
 	}
 
@@ -80,7 +85,7 @@ func (r *RedisCli) Shorten(url string, exp int64) (string, error) {
 
 func (r *RedisCli) ShortlinkInfo(eid string) (interface{}, error) {
 	d, err := r.Cli.Get(fmt.Sprintf(ShortlinkDetailKey, eid)).Result()
-	if err != redis.Nil {
+	if err == redis.Nil {
 		return "", StatusError{404, errors.New("Unknow short URL")}
 	} else if err != nil {
 		return "", err
@@ -116,4 +121,23 @@ func NewRedisCli(add string, passwd string, db int) *RedisCli {
 		panic(err)
 	}
 	return &RedisCli{Cli: c}
+}
+
+func toMd5(data string) string {
+	md5 := md5.New()
+	md5.Write([]byte(data))
+	md5Data := md5.Sum([]byte(""))
+	return hex.EncodeToString(md5Data)
+}
+
+func toHmac(key, data string) string {
+	hmac := hmac.New(md5.New, []byte(key))
+	hmac.Write([]byte(data))
+	return hex.EncodeToString(hmac.Sum([]byte("")))
+}
+
+func toSha1(data string) string {
+	sha1 := sha1.New()
+	sha1.Write([]byte(data))
+	return hex.EncodeToString(sha1.Sum([]byte("")))
 }

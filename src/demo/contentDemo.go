e607f5403b90b3489258561ca812e56e7a19b5a0
cdy1996 <chendongyi1996@gmail.com>
2020-02-26 11:17:38 +0800

    说明:  go context代码

--- src/demo/contentDemo.go
+++ src/demo/contentDemo.go
@@ -3,28 +3,68 @@ package main
 import (
 	"context"
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func main() {
 	ctx, cancel := context.WithCancel(context.Background())
-	go func(ctx context.Context) {
+	//go func(ctx context.Context) {
+	//	for {
+	//		select {
+	//		case <-ctx.Done():
+	//			fmt.Println("监控退出，停止了...")
+	//			return
+	//		default:
+	//			fmt.Println("goroutine监控中...")
+	//			time.Sleep(2 * time.Second)
+	//		}
+	//	}
+	//}(ctx)
+	//
+	//time.Sleep(10 * time.Second)
+	//fmt.Println("可以了，通知监控停止")
+	//cancel()
+	////为了检测监控过是否停止，如果没有监控输出，就表示停止了
+	//time.Sleep(5 * time.Second)
+
+	eatNum := chiHanBao(ctx)
+	for n := range eatNum {
+		if n >= 10 {
+			cancel()
+			break
+		}
+	}
+
+	fmt.Println("正在统计结果。。。")
+	time.Sleep(1 * time.Second)
+
+}
+
+func chiHanBao(ctx context.Context) <-chan int {
+	c := make(chan int)
+	// 个数
+	n := 0
+	// 时间
+	t := 0
+	go func() {
 		for {
+			//time.Sleep(time.Second)
 			select {
 			case <-ctx.Done():
-				fmt.Println("监控退出，停止了...")
+				fmt.Printf("耗时 %d 秒，吃了 %d 个汉堡 \n", t, n)
 				return
-			default:
-				fmt.Println("goroutine监控中...")
-				time.Sleep(2 * time.Second)
+			case c <- n:
+				incr := rand.Intn(5)
+				n += incr
+				if n >= 10 {
+					n = 10
+				}
+				t++
+				fmt.Printf("我吃了 %d 个汉堡\n", n)
 			}
 		}
-	}(ctx)
+	}()
 
-	time.Sleep(10 * time.Second)
-	fmt.Println("可以了，通知监控停止")
-	cancel()
-	//为了检测监控过是否停止，如果没有监控输出，就表示停止了
-	time.Sleep(5 * time.Second)
-
+	return c
 }

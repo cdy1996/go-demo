1831a54ae0cd9c0549afdc1c6f7898c8061af7da
cdy1996 <chendongyi1996@gmail.com>
2020-06-15 14:54:29 +0800

    说明:  leetcode 三数之和

--- src/leetcode/leetcode15.go
+++ src/leetcode/leetcode15.go
@@ -6,46 +6,77 @@ import (
 )
 
 /**
-未完成
+ 存在重复的情况
+     -1 -1  -1  2  2
+	 -1 -1  0  1  1
+     0 0 0
+
+
 */
 func main() {
-	fmt.Printf("%v", threeSum([]int{-1, 0, 1, 2, -1, -4}))
+	fmt.Printf("%v\n", threeSum([]int{-1, 0, 1, 2, -1, -4})) // -4 -1 -1 0 1 2
+	fmt.Printf("%v\n", threeSum([]int{0, 0, 0}))
+	fmt.Printf("%v\n", threeSum([]int{-1, -1, -1, 2, 2}))
+	fmt.Printf("%v", threeSum([]int{-1, -1, 0, 1, 1}))
 
 }
 func threeSum(nums []int) [][]int {
-	maps := make(map[int][]int)
 	result := make([][]int, 0)
 	s.Ints(nums)
-	for i, num := range nums {
-		v, b := maps[num]
-		if !b {
-			maps[num] = []int{i}
-		} else {
-			maps[num] = append(v, i)
-		}
-	}
 	l := len(nums)
-	for i, num := range nums {
-		if l-i == 1 {
+	for i := 0; i < l; {
+		if i >= l-2 {
 			break
 		}
-		for j := i + 1; j < l; j++ {
-			b, v := maps[-num-nums[j]]
-			if v {
-				for i2 := range b {
-					if i < j && j < b[i2] {
-						temp := make([]int, 3)
-						temp[0] = nums[i]
-						temp[1] = nums[j]
-						temp[2] = nums[b[i2]]
-
-						result = append(result, temp)
+		for j, k := i+1, l-1; j < l || k > j; {
+			temp := nums[j] + nums[k] + nums[i]
+			if temp == 0 && j != k {
+				temp := make([]int, 3)
+				temp[0] = nums[i]
+				temp[1] = nums[j]
+				temp[2] = nums[k]
+				result = append(result, temp)
+				// 选取成功后  继续推进, 并且过滤掉有和重复的
+				for {
+					if j < l-1 && nums[j] == nums[j+1] {
+						j++
+					} else {
 						break
 					}
 				}
+				for {
+					if k > 1 && nums[k] == nums[k-1] {
+						k--
+					} else {
+						break
+					}
+				}
+			}
 
+			if temp > 0 {
+				k--
+			} else if temp < 0 {
+				j++
+			} else {
+				k--
+				j++
+			}
+
+			if j >= k {
+				break
+			}
+
+		}
+
+		for {
+			if i < l-1 && nums[i] == nums[i+1] {
+				i++
+			} else {
+				break
 			}
 		}
+		i++
+
 	}
 	return result
 }
